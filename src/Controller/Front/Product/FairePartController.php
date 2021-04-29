2e3bd05d9ec9a9a71c30b858139bd7d1919e9efe
Nicolas F <nicolasferchaud@hotmail.fr>
2021-04-29 16:18:45 +0200

    detail faire part

--- src/Controller/Front/Product/FairePartController.php
+++ src/Controller/Front/Product/FairePartController.php
@@ -27,9 +27,14 @@ class FairePartController extends AbstractController
     /**
      * @Route("/faire_part/birth", name="public/faire_part/birth")
      */
-    public function FairePartBirth()
+    public function FairePartBirth(ProductRepository $productRepository)
     {
+        $birth = $productRepository->findBy(
+            ['category' => 93]
+        );
+
         return $this->render('Public/faire_part/birth.html.twig',[
+            'faire_parts' => $birth,
             'current_menu' => 'faire_part'
         ]);
     }
@@ -37,9 +42,26 @@ class FairePartController extends AbstractController
     /**
      * @Route("/faire_part/baptism", name="public/faire_part/baptism")
      */
-    public function FairePartBaptism()
+    public function FairePartBaptism(ProductRepository $productRepository)
     {
+        $baptism = $productRepository->findBy(
+            ['category' => 94]
+        );
+
         return $this->render('Public/faire_part/baptism.html.twig',[
+            'faire_parts' => $baptism,
+            'current_menu' => 'faire_part'
+        ]);
+    }
+
+    /**
+     * @Route("/faire_part/{id}", name="public/faire_part/show")
+     */
+    public function show($id, ProductRepository $productRepository)
+    {
+        $detail = $productRepository->find($id);
+        return $this->render('Public/faire_part/detail.html.twig',[
+            'faire_part' => $detail,
             'current_menu' => 'faire_part'
         ]);
     }
